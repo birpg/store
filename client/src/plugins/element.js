661a56024bb48b142063a6c6227fd5ac7c45fd30
DESKTOP-VA5VPT9\Admin <1730390337@qq.com>
2021-04-16 12:41:43 +0800

    新增yran、nodemon，优化element-ui引入

--- client/src/plugins/element.js
+++ client/src/plugins/element.js
@@ -30,30 +30,36 @@ import {
   Tabs,
 } from 'element-ui'
 
-Vue.use(Button)
-Vue.use(Card)
-Vue.use(Input)
-Vue.use(Table)
-Vue.use(TableColumn)
-Vue.use(Row)
-Vue.use(Col)
-Vue.use(FormItem)
-Vue.use(Form)
-Vue.use(Dialog)
-Vue.use(Pagination)
-Vue.use(Menu)
-Vue.use(MenuItem)
-Vue.use(Submenu)
-Vue.use(Icon)
-Vue.use(Select)
-Vue.use(Dropdown)
-Vue.use(DropdownMenu)
-Vue.use(DropdownItem)
-Vue.use(Radio)
-Vue.use(Option)
-Vue.use(DatePicker)
-Vue.use(TimePicker)
-Vue.use(Checkbox)
-Vue.use(CheckboxGroup)
-Vue.use(TabPane)
-Vue.use(Tabs)
+const components = [
+  Button,
+  Card,
+  Input,
+  Table,
+  TableColumn,
+  Row,
+  Col,
+  Form,
+  FormItem,
+  Dialog,
+  Pagination,
+  Menu,
+  MenuItem,
+  Submenu,
+  Icon,
+  Select,
+  Dropdown,
+  DropdownMenu,
+  DropdownItem,
+  Radio,
+  Option,
+  DatePicker,
+  TimePicker,
+  Checkbox,
+  CheckboxGroup,
+  TabPane,
+  Tabs,
+]
+
+components.map(component => {
+  return Vue.use(component)
+})
